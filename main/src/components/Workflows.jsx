b45d22a24b841d47573b257951b192f66f616c9e
AlexPhysics <costritaalex@gmail.com>
2025-05-11 17:52:40 +0200

    -

--- main/src/components/Workflows.jsx
+++ main/src/components/Workflows.jsx
@@ -3,8 +3,8 @@ import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import { expCards } from "../constants";
-import TitleHeader from "./TitleHeader";
-import GlowCard from "./GlowCard";
+import TitleHeader from "../components/TitleHeader";
+import GlowCard from "../components/GlowCard";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -91,12 +91,13 @@ const Workflow = () => {
 
   return (
     <section
-      id="workflow"
+      id="experience"
       className="flex-center md:mt-40 mt-20 section-padding xl:px-0"
     >
       <div className="w-full h-full md:px-20 px-5">
         <TitleHeader
-          title="Customer journey"
+          title="Professional Work Experience"
+          sub="💼 My Career Overview"
         />
         <div className="mt-32 relative">
           <div className="relative z-50 xl:space-y-32 space-y-10">
@@ -123,6 +124,9 @@ const Workflow = () => {
                         <h1 className="font-semibold text-3xl">{card.title}</h1>
                         <p className="my-5 text-white-50">
                           🗓️&nbsp;{card.date}
+                        </p>
+                        <p className="text-[#839CB5] italic">
+                          Responsibilities
                         </p>
                         <ul className="list-disc ms-5 mt-5 flex flex-col gap-5 text-white-50">
                           {card.responsibilities.map(
--- main/src/utils/index.js
+++ main/src/utils/index.js
@@ -1,7 +1,3 @@
-import hero from "/assets/images/hero.jpeg";
-
-export const heroImg = hero;
-
 import hmv from "/assets/videos/hero.mp4";
 import smallmv from "/assets/videos/smallHero.mp4";
 import highlight1 from "/assets/videos/highlight1.mp4";
